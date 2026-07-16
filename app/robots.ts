64f64fcd413b773a32373731cf268164fe42c99d
Prasham Ghimire <prashamghimire666@gmail.com>
2026-07-17 02:11:51 +0545

    Add sitemap and robots.txt

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://prashamghimire.com.np/sitemap.xml",
+  };
+}
