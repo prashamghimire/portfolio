378b7cedc6bcd823f830667eed8044125c0f34b0
Prasham Ghimire <prashamghimire666@gmail.com>
2026-07-17 10:48:42 +0545

    Integrate Google Analytics 4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import GoogleAnalytics from "@/components/GoogleAnalytics";
 
 import PersonSchema from "@/components/seo/PersonSchema";
 
@@ -94,6 +95,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <PersonSchema />
+        <GoogleAnalytics />
         {children}
       </body>
     </html>
