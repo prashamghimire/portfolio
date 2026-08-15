800be6edebc85036aa17ffd003962037088316ff
Prasham Ghimire <prashamghimire666@gmail.com>
2026-08-15 08:56:42 +0545

    ivc-igb added

--- data/leadership/innovation.ts
+++ data/leadership/innovation.ts
@@ -25,4 +25,27 @@ export const innovationAchievements: Achievement[] = [
     ],
   },
 
-];
+  {
+    title: "International Virtual Conference - Innovative Green Business (IVC-IGB2026)",
+
+    category: "Entrepreneurship",
+
+    date: "2026/07/12",
+
+    organizer: "Southwestern Group, Whitehouse International College, Kuala Lampur University of Science and Technology (KLUST)",
+
+    location: "Virtual",
+
+    summary:
+      "Participated in International Virtual Conference - Innovative Green Business (IVC-IGB2026).",
+
+    description:
+      "Pitched a business idea 'ReCraft Nepal' at the International Virtual Conference - Innovative Green Business (IVC-IGB2026) organized by Southwestern Group, Whitehouse International College, Kuala Lampur University of Science and Technology (KLUST). The conference provided a platform to showcase innovative business ideas and connect with like-minded individuals from around the world.",
+
+    images: [
+      "/journey/leadership/ivc-igb/1.jpg",
+      "/journey/leadership/ivc-igb/2.jpg",
+      "/journey/leadership/ivc-igb/3.jpg"
+    ],
+  },
+]
